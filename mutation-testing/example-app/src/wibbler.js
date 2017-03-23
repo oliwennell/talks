6b1d65f4b681503de6116f66224eb5ee3d0e9a1c
Oli Wennell <oli.wennell@gmail.com>
2017-03-23 21:01:07 +0000

    Using password validation as the example

--- mutation-testing/example-app/src/wibbler.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    wibbleIt: function(input) {
-        if (input.length > 0 && input.length < 10) {
-            return input + " wibble";
-        }
-
-        return "wibble";
-    }
-};
