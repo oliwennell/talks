5a433073ae630b3bc2f55ad5e9300f5c73b39a4e
Oli Wennell <oli.wennell@gmail.com>
2017-05-09 09:33:25 +0100

    Setting max length to 10 so it's a round number

--- mutation-testing/example-app/src/password-validation.js
+++ mutation-testing/example-app/src/password-validation.js
@@ -2,7 +2,7 @@ module.exports = {
 
     validate: function(password) {
 
-        if (password.length > 0 && password.length < 10) {
+        if (password.length > 0 && password.length < 11) {
             return true;
         }
 
